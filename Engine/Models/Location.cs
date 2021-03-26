0535a7f336f649fecb28fbe3fa76488b6f9224c9
15vbasi <15vbasi@ROOM110-15>
2021-03-26 14:11:39 +0000

    Lesson 7.2 [in progress]

--- Engine/Models/Location.cs
+++ Engine/Models/Location.cs
@@ -34,5 +34,27 @@ namespace Engine.Models
             get;
             set;
         } = new List<Quest>();
+        public List<MonsterEncounter> MonstersHere
+        {
+            get;
+            set;
+        } = new List<MonsterEncounter>();
+
+        public void AddMonster(int _monsterid, int _encounterchance)
+        {
+            if (MonstersHere.Exists(m => m.MONSTERID == _monsterid))
+            {
+                //This monster has already been added to this location
+                //So overwrite the chance with the new number
+                MonstersHere.First(m => m.MONSTERID = _monsterid)
+                            .ENCOUNTERCHANCE = _encounterchance;
+            }
+            else
+            {
+                //This monster is not already at this location so add it
+                MonstersHere.Add(new MonsterEncounter(_monsterid, _encounterchance));
+            }
+
+        }
     }
 }
---
+++ Engine/Models/MonsterEncounter.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Engine.Models
+{
+    public class MonsterEncounter
+    {
+        public int MONSTERID
+        {
+            get;
+            set;
+        }
+        public int ENCOUNTERCHANCE
+        {
+            get;
+            set;
+        }
+
+        public MonsterEncounter(int _monsterid, int _encounterchance)
+        {
+            MONSTERID = _monsterid;
+            ENCOUNTERCHANCE = _encounterchance;
+        }
+    }
+}
