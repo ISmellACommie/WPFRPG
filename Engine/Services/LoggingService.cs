7540d4bfa2a913b7c55abee0e11ab8c294e4425b
Viraaj Basi <viraajbasi@gmail.com>
2021-04-13 14:51:26 +0100

    15.2

---
+++ Engine/Services/LoggingService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Engine.Services
+{
+    public static class LoggingService
+    {
+        private const string LOG_FILE_DIRECTORY = "Logs";
+
+        static LoggingService()
+        {
+            string logDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, LOG_FILE_DIRECTORY);
+
+            if (!Directory.Exists(logDirectory))
+            {
+                Directory.CreateDirectory(logDirectory);
+            }
+        }
+
+        public static void Log(Exception e, bool isInnerException = false)
+        {
+            using (StreamWriter sw = new StreamWriter(LogFileName(), true))
+            {
+                sw.WriteLine(isInnerException ? "INNER EXCEPTION" : $"EXCEPTION: {DateTime.Now}");
+                sw.WriteLine(new string(isInnerException ? '-' : '=', 40));
+                sw.WriteLine($"{e.Message}");
+                sw.WriteLine($"{e.StackTrace}");
+                sw.WriteLine();
+            }
+
+            if(e.InnerException != null)
+            {
+                Log(e.InnerException, true);
+            }
+        }
+
+        private static string LogFileName()
+        {
+            return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, LOG_FILE_DIRECTORY, $"WPFRPG_{DateTime.Now:yyyMMdd}.log");
+        }
+    }
+}
